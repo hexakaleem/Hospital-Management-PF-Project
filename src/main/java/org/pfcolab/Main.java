2c387d40ec57b8687ea2fb1cc5f4eb37f912030b
Ahmed Mujtaba <ahmedmujtaba.ult@gmail.com>
2023-12-14 11:28:36 +0500

    New changes: 1- Added doctors data Manupilation functions 2- Added pasword checker 3- Added new navigation for admin portal removing the Switch statement with a single statement

--- src/main/java/org/pfcolab/Main.java
+++ src/main/java/org/pfcolab/Main.java
@@ -1,11 +1,6 @@
 package org.pfcolab;
-
-import javax.lang.model.element.NestingKind;
-import java.io.File;
-import java.util.ArrayList;
 import java.util.*;
-
-
+import java.lang.*;
 /*
 
  * What the system should do?
@@ -32,47 +27,82 @@ import java.util.*;
 
  * */
 
-
 public class Main {
+
     static Scanner scanner = new Scanner(System.in);
-    
+
     final static String DATABASE = "HospitalData";
+    //Pasword for admin
+    static String savedAdminPassword = "kaleem";
     static String currentDir = System.getProperty("user.dir");
     final static String databaseFilePath = currentDir + "//" + DATABASE + ".accdb";
-  
-    //Doctors lists
-    static ArrayList<String> doctorsNamesList = new ArrayList<String>();
-    static ArrayList<String> doctorsWardsList = new ArrayList<String>();
-    static ArrayList<Integer> doctorsContactsList = new ArrayList<Integer>();
-    
-    //Receptionist lists
-    static ArrayList<String> receptionistNamesList = new ArrayList<String>();
-    static ArrayList<Integer> receptionistPaswordList = new ArrayList<Integer>();
-    
-    //Ward lists
-    static ArrayList<String> wardNameList = new ArrayList<String>();
-    //needed to be predecleared since there are only a few type of wards.
-    static ArrayList<String> wardTypeList = new ArrayList<String>();
-    static ArrayList<String> wardTotalBedsList = new ArrayList<String>();
-    static ArrayList<String> wardAvailableBedsList = new ArrayList<String>();
-    static ArrayList<String> wardInUseBedsList = new ArrayList<String>();
+
+    //Array lists for the stored data of multiple entries
+    static ArrayList<String[]> doctorsList = new ArrayList<>();
+    static ArrayList<String[]> receptionitsList = new ArrayList<>();
+    static ArrayList<String[]> wardsList = new ArrayList<>();
+    static ArrayList<String[]> patientsList = new ArrayList<>();
+    static ArrayList<String[]> diagnosisList = new ArrayList<>();
+    static ArrayList<String[]> appointmentsList = new ArrayList<>();
 
 
-    
-    public static void handleUserInput() {
 
+    //main function! wow!! ;)
+    public static void main(String[] args) {
+
+        mainPortal();
+
+    }
+
+    //function to display options of the main portal
+    static void displayOptionsMainPortal() {
+        System.out.println("\n=== Menu ===");
+        System.out.println("1. Navigate to Admin Portal ");
+        System.out.println("2. Navigate to Receptionist Portal");
+        System.out.println("3. Navigate to Doctors Portal");
+        System.out.println("4. Exit");
+        System.out.print("Enter your choice: ");
+    }
+
+    //function to display options of the admin portal
+    static void displayOptionsAdminPortal() {
+        //Doctors data options
+        System.out.println("\n=== Admin Portal Menu ===");
+        System.out.println("1. Add Doctors Data");
+        System.out.println("2. Edit Doctors Data");
+        System.out.println("3. Delete Doctors Data");
+        System.out.println("4. Display all Doctors");
+        System.out.println("5. Display Specific Doctor's Data");
+
+        //Receptionist data options
+        System.out.println("6. Add Receptionists");
+        System.out.println("7. Edit Receptionists");
+        System.out.println("8. Delete Receptionists");
+        System.out.println("9. Display Receptions");
+
+        //Ward data options
+        System.out.println("10. Add Ward");
+        System.out.println("11. Edit Wards");
+        System.out.println("12. Delete Wards");
+        System.out.println("13. Display Wards");
+        System.out.println("14. Return to Main Menu");
+    }
+
+    //main portal to ask the user to be directed to.
+    public static void mainPortal() {
+        displayOptionsMainPortal();
         int choice = scanner.nextInt();
-
+        scanner.nextLine();
         switch (choice) {
             case 1:
-                // Navigate to Reception Portal
+                // Navigate to Admin Portal
+                adminPortal();
                 break;
             case 2:
                 // Navigate to Doctors Portal
                 break;
             case 3:
-                //Navigate to the case statement handelling the admin portal
-                adminPortal();
+                // Navigate to the case statement handling the receptionist portal
                 break;
             case 4:
                 // Exit the system
@@ -82,83 +112,258 @@ public class Main {
                 break;
         }
     }
-   
-    public static void adminPortal(){
-        int choice = scanner.nextInt();
-        //Making another switch statement for the admin portal. The admin will choose from three options
-        //1-Edit Doctors data 2- Edit Recptionists Data 3- Edit Wards Data
-        System.out.println("1. Edit Doctors Data");
-        System.out.println("2. Edit Receptionists Data");
-        System.out.println("3. Edit Wards Data");
 
-        switch (choice){ 
+    //Now we are making a function for admin login it works only if the user has the correct pasword
+    //and login Name.
+    static boolean isAdmin(){
 
-        case 1:
-        //Edit doctor data    
-       
-        System.out.println("1. Add Doctor");
-        System.out.println("2. Edit Doctor");
-        int choiceDoc = scanner.nextInt();
-        switch (choiceDoc) {
+        System.out.println("Enter your Password: ");
+        String enteredAdminPassword = scanner.nextLine();
+        System.out.println(enteredAdminPassword);
+        System.out.println(savedAdminPassword);
+        return(enteredAdminPassword.equals(savedAdminPassword));
+
+    }
+
+    static void paswordChecker(){
+
+        //Checking if the password is correct
+        int loginTries = 0;
+        while (isAdmin() != true) {
+            System.out.println("Wrong Password!");
+            loginTries++;
+            if (loginTries == 5) {
+                System.out.println("You have entered wrong pasword 5 times, returing to Main Menu");
+                mainPortal();
+            }
+        }
+    }
+
+    //This is the admin portal. It has the ability to add and remove doctors, receptionists and wards
+    public static void adminPortal() {
+
+        //checking password
+        paswordChecker();
+        //diplaying options for admin portal
+        displayOptionsAdminPortal();
+        //inputing user for admin portal options
+        System.out.print("Enter your choice: ");
+        int choiceAdminPortal = scanner.nextInt();
+
+        switch (choiceAdminPortal) {
             case 1:
-                System.out.print("Enter Doctor Name: ");
-                String docName = scanner.nextLine();
-                System.out.print("Enter Doctor Ward: ");
-                String docWard = scanner.nextLine();
-                System.out.print("Enter Doctor contact: ");
-                String docContact = scanner.next();
-                String docInfo = docName + "," + docWard + "," + docContact;
-                addDoctor(docInfo);
+                System.out.println("You selected: Add Doctors Data");
+                // Add your logic for this option
+                addDoctorFromUserInput(doctorsList, scanner);
+
                 break;
             case 2:
-
+                System.out.println("You selected: Edit Doctors Data");
+                // Add your logic for this option
                 break;
+            case 3:
+                System.out.println("You selected: Delete Doctors Data");
+                // Add your logic for this option
+                break;
+            case 4:
+                System.out.println("You selected: Display all Doctors");
+                // Add your logic for this option
+                break;
+            case 5:
+                System.out.println("You selected: Display Specific Doctor's Data");
+                // Add your logic for this option
+                break;
+            case 6:
+                System.out.println("You selected: Add Receptionists");
+                // Add your logic for this option
+                break;
+            case 7:
+                System.out.println("You selected: Edit Receptionists");
+                // Add your logic for this option
+                break;
+            case 8:
+                System.out.println("You selected: Delete Receptionists");
+                // Add your logic for this option
+                break;
+            case 9:
+                System.out.println("You selected: Display Receptions");
+                // Add your logic for this option
+                break;
+            case 10:
+                System.out.println("You selected: Add Ward");
+                // Add your logic for this option
+                break;
+            case 11:
+                System.out.println("You selected: Edit Wards");
+                // Add your logic for this option
+                break;
+            case 12:
+                System.out.println("You selected: Delete Wards");
+                // Add your logic for this option
+                break;
+            case 13:
+                System.out.println("You selected: Display Wards");
+                // Add your logic for this option
+                break;
+            case 14:
+                System.out.println("You selected: Return to Main Menu");
+                mainPortal();
+                break;
+            default:
+                System.out.println("Invalid choice");
         }
-    }}
-    public static void displayMainMenu() {
-        System.out.println("1. Reception Portal");
-        System.out.println("2. Doctors Portal");
-        System.out.println("3. Admin Portal");
-        System.out.println("4. Exit");
     }
-    public static void firstTimeRun(){
-        //Create table in the database
-        //Create files (doctors, receptionists , patients, wards, appointments, )
-        String databaseURL = "jdbc:ucanaccess://" + databaseFilePath + ";newdatabaseversion=V2010";
+    /* Functions to manipulate doctor database are below:
+    These include add, edit, delete, display all, display specific doctors
+    Use with care ;)
+     */
 
+    // Add a new doctor to the database based on user input
+    private static void addDoctorFromUserInput(ArrayList<String[]> doctorsList, Scanner scanner) {
+        System.out.println("\nEnter details for the new doctor:");
+        System.out.print("Enter doctor name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter doctor timing: ");
+        String timing = scanner.nextLine();
+        System.out.print("Enter doctor ward: ");
+        String ward = scanner.nextLine();
+        System.out.print("Enter doctor specialty: ");
+        String specialty = scanner.nextLine();
+
+        // Creating a new array to store doctor details
+        String[] newDoctor = {name, timing, ward, specialty};
+
+        // Adding the new doctor to the ArrayList
+        doctorsList.add(newDoctor);
+
+        System.out.println("New doctor added successfully.");
     }
 
-    public static void main(String[] args) {
-        displayMainMenu();
-    }
-
-    public static boolean isNumeric(String inputS) {
-        return inputS.chars().allMatch(Character::isDigit);
-    }
-
-    public static boolean fileExists(String filePath) {
-        File file = new File(filePath);
-        return file.exists();
-    }
-
-    public static void addDoctor(String doctorInfo) {
-        String[] doctorData = doctorInfo.split(",");
-        String name = doctorData[0];
-        String ward = doctorData[1];
-        String contactString = doctorData[2];
-        if (isNumeric(contactString)) {
-            int contactInt = Integer.parseInt(contactString);
-            addDoctor(name, ward, contactInt);
+    // Display all doctors in the database
+    private static void displayAllDoctors(ArrayList<String[]> doctorsList) {
+        System.out.println("\nList of Doctors:");
+        for (int i = 0; i < doctorsList.size(); i++) {
+            System.out.println("Doctor ID: " + (i + 1) + ", Doctor Info: " + arrayToString(doctorsList.get(i)));
         }
     }
 
-    public static void addDoctor(String name, String ward, int contact) {
-        try {
-            doctorsNamesList.add(name);
-            doctorsWardsList.add(ward);
-            doctorsContactsList.add(contact);
-        } catch (Exception e) {
-            e.printStackTrace();
+    // View specific doctor details based on user input (by name)
+    private static void viewSpecificDoctorDetails(ArrayList<String[]> doctorsList, Scanner scanner) {
+        System.out.print("\nEnter the name of the doctor to view details: ");
+        String doctorNameToView = scanner.nextLine();
+
+        // Check if the doctor name exists in the database
+        int doctorIndexToView = getDoctorIndexByName(doctorsList, doctorNameToView);
+
+        if (doctorIndexToView != -1) {
+            // View the details of the specific doctor
+            String[] doctorDetails = doctorsList.get(doctorIndexToView);
+            System.out.println("Details of Doctor '" + doctorNameToView + "': " + arrayToString(doctorDetails));
+        } else {
+            System.out.println("Doctor with name '" + doctorNameToView + "' not found in the database.");
         }
     }
+
+    // Edit details of a doctor based on user input (by name)
+    private static void editDoctorDetails(ArrayList<String[]> doctorsList, Scanner scanner) {
+        System.out.print("\nEnter the name of the doctor to edit details: ");
+        String doctorNameToEdit = scanner.nextLine();
+
+        // Check if the doctor name exists in the database
+        int doctorIndexToEdit = getDoctorIndexByName(doctorsList, doctorNameToEdit);
+
+        if (doctorIndexToEdit != -1) {
+            // Edit the details of the specific doctor
+            String[] doctorDetails = doctorsList.get(doctorIndexToEdit);
+            System.out.println("Current Details of Doctor '" + doctorNameToEdit + "': " + arrayToString(doctorDetails));
+
+            System.out.println("\nSelect the option to edit:");
+            System.out.println("1. Edit Timing");
+            System.out.println("2. Edit Ward");
+            System.out.println("3. Edit Specialty");
+            System.out.print("Enter your choice: ");
+            int editChoice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
+
+            switch (editChoice) {
+                case 1:
+                    System.out.print("Enter new timing: ");
+                    String newTiming = scanner.nextLine();
+                    doctorDetails[1] = newTiming;
+                    break;
+                case 2:
+                    System.out.print("Enter new ward: ");
+                    String newWard = scanner.nextLine();
+                    doctorDetails[2] = newWard;
+                    break;
+                case 3:
+                    System.out.print("Enter new specialty: ");
+                    String newSpecialty = scanner.nextLine();
+                    doctorDetails[3] = newSpecialty;
+                    break;
+                default:
+                    System.out.println("Invalid edit choice. No changes made.");
+            }
+
+            System.out.println("Details of Doctor '" + doctorNameToEdit + "' after edit: " + arrayToString(doctorDetails));
+        } else {
+            System.out.println("Doctor with name '" + doctorNameToEdit + "' not found in the database.");
+        }
+    }
+
+    // Delete a doctor from the database based on user input (by name)
+    private static void deleteDoctorByNameFromUserInput(ArrayList<String[]> doctorsList, Scanner scanner) {
+        System.out.print("\nEnter the name of the doctor to delete: ");
+        String doctorNameToDelete = scanner.nextLine();
+
+        // Check if the doctor name exists in the database
+        int doctorIndexToDelete = getDoctorIndexByName(doctorsList, doctorNameToDelete);
+
+        if (doctorIndexToDelete != -1) {
+            // Delete the doctor
+            doctorsList.remove(doctorIndexToDelete);
+            System.out.println("Doctor with name '" + doctorNameToDelete + "' has been deleted.");
+        } else {
+            System.out.println("Doctor with name '" + doctorNameToDelete + "' not found in the database.");
+        }
+    }
+
+    // View the ID of a doctor by name
+    private static void viewDoctorIdByName(ArrayList<String[]> doctorsList, Scanner scanner) {
+        System.out.print("\nEnter the name of the doctor to view ID: ");
+        String doctorNameToViewId = scanner.nextLine();
+
+        // Check if the doctor name exists in the database
+        int doctorIndexToViewId = getDoctorIndexByName(doctorsList, doctorNameToViewId);
+
+        if (doctorIndexToViewId != -1) {
+            System.out.println("Doctor ID for '" + doctorNameToViewId + "': " + (doctorIndexToViewId + 1));
+        } else {
+            System.out.println("Doctor with name '" + doctorNameToViewId + "' not found in the database.");
+        }
+    }
+
+    // Get the index of a doctor by name
+    private static int getDoctorIndexByName(ArrayList<String[]> doctorsList, String name) {
+        for (int i = 0; i < doctorsList.size(); i++) {
+            if (name.equals(doctorsList.get(i)[0])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // Helper method to convert an array to a string
+    private static String arrayToString(String[] array) {
+        StringBuilder result = new StringBuilder("[");
+        for (int i = 0; i < array.length; i++) {
+            result.append(array[i]);
+            if (i < array.length - 1) {
+                result.append(", ");
+            }
+        }
+        result.append("]");
+        return result.toString();
+    }
+
 }
